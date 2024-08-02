91a5baa05f41ebb89760182ebd707fde068df1fa
Jefferson Timotius Mansur <122242388+JeffTimoman@users.noreply.github.com>
2024-08-02 09:49:31 +0700

    Init Push

---
+++ constants/icons.js
@@ -0,0 +1,14 @@
+import bookmark from '../assets/icons/bookmark.png';
+import eye_hide from '../assets/icons/eye-hide.png';
+import eye from '../assets/icons/eye.png';
+import left_arrow from '../assets/icons/left-arrow.png';
+import logout from '../assets/icons/logout.png';
+import menu from '../assets/icons/menu.png';
+import play from '../assets/icons/play.png';
+import plus from '../assets/icons/plus.png';
+import right_arrow from '../assets/icons/right-arrow.png';
+import search from '../assets/icons/search.png';
+import settings from '../assets/icons/settings.png';
+import upload from '../assets/icons/upload.png';
+
+export default {bookmark, eye_hide, eye, left_arrow, logout, menu, play, plus, right_arrow, search, settings, upload};
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import logo from "../assets/images/logo.png";
+import cards from "../assets/images/cards.png";
+import profile_background from "../assets/images/profile_background.png";
+
+export default {logo, cards, profile_background};
